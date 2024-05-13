47f656a257cfa6df30f5311a9f0ad2707f6f3bc0
Jeremy Fabian <118284208+LkyFabian@users.noreply.github.com>
2024-05-13 08:23:08 +0700

    Add files via upload
    "Buat pembejaran saja"

---
+++ MOD12_1302220143/Form1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MOD12_1302220143
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int CariNilaiPangkat(int a, int b)
+        {
+            if (b == 0)
+            {
+                return 0;
+            }
+            if (b < 0)
+            {
+                return -1;
+            }
+            if (b > 10)
+            {
+                return -2;
+            }
+            int hasil = a;
+
+            for (int i = 1; i < b; i++)
+            {
+                if (hasil > int.MaxValue / a)
+                {
+                    return -3;
+                }
+                hasil *= a;
+            }
+            return hasil;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            label1.Text = $"pangkat:{CariNilaiPangkat(a,b).ToString()}";    
+        }
+    }
+}
---
+++ MOD12_1302220143Tests/Form1Tests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MOD12_1302220143;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD12_1302220143.Tests
+{
+    [TestClass()]
+    public class Form1Tests
+    {
+        [TestMethod()]
+        public void CariNilaiPangkatTest()
+        {
+            int a = 2;
+            int b = 2;
+            int expected = 4;
+            int actual = Form1.Equals;
+
+            Assert.AreEqual(Form1.CariNilaiPangkat(a, b));
+        }
+        public void CariNilaiPangkatTest1()
+        {
+            int a = 12;
+            int b = 12;
+            int expected = -3;
+            int actual = Form1.Equals;
+
+            Assert.AreEqual(Form1.CariNilaiPangkat(a, b));
+        }
+    }
+}
